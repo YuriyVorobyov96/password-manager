2438a78e72f4dc98d4c227c3c9495d7a73a26e3b
YuriyVorobyov96 <darthrevan1333@gmail.com>
2024-12-25 10:53:22 +0300

    add password logic

--- cipher/password.go
+++ cipher/password.go
@@ -28,3 +28,23 @@ func CreateMasterPassword(password string) {
 	files.WriteFile([]byte(hashedPassword), masterPasswordFileName)
 	color.Green("Successfully add master password")
 }
+
+func CheckMasterPassword(password string) bool {
+	hash, err := files.ReadFile(masterPasswordFileName)
+
+	if err != nil {
+		color.Red("Master password data is broken. Please restart Vault")
+	}
+
+	isMatch := CheckHash(password, string(hash))
+
+	if isMatch {
+		color.Green("Correct password")
+
+		return isMatch
+	}
+
+	color.Red("Incorrect password")
+
+	return isMatch
+}
